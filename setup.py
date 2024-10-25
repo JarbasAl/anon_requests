1d86bcdf8541045376516503039b8e50933887bf
miro <jarbasai@mailfence.com>
2024-10-25 23:06:30 +0100

    feat:semver

--- setup.py
+++ setup.py
@@ -1,17 +1,68 @@
+import os
+import os.path
+
 from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def package_files(directory):
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+def get_version():
+    version_file = os.path.join(BASEDIR, 'anon_requests', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if int(alpha):
+        version += f"a{alpha}"
+    return version
+
+
+def get_description():
+    with open(os.path.join(BASEDIR, "README.md"), "r") as f:
+        long_description = f.read()
+    return long_description
+
 
 setup(
     name='anon_requests',
-    version='0.0.1',
+    version=get_version(),
     packages=['anon_requests', 'anon_requests.proxy_sources'],
     url='https://github.com/OpenJarbas/anon_requests',
-    install_requires=["PySocks>=1.5.7",
-                      "requests>=2.11.0",
-                      "stem>=1.4.0",
-                      "bs4",
-                      "requests_cache"],
+    install_requires=required('requirements.txt'),
     license='apache-2.0',
     author='jarbasAi',
     author_email='jarbasai@mailfence.com',
-    description='anonymous requests'
+    long_description=get_description(),
+    long_description_content_type="text/markdown"
 )
